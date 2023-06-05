8fefb1e01d2c5626eb6b46182672a22876fa6575
tesseractx0FFFF <itkugit321@gmail.com>
2023-06-05 21:48:21 +0300

    added a main iife

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import Project from './newProject.js';
+// create the default project that will show when the app is launched for the first time.
 const main = (() => {
-
+  const projectArray = [];
+  const defaultProject = new Project('default');
+  return { projectArray, defaultProject };
 })();
+
+console.log(main.defaultProject);
