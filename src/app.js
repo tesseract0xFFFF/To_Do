91b4caa5eaa7f57d7e48844baa621887a218bd87
tesseractx0FFFF <itkugit321@gmail.com>
2023-06-08 23:59:05 +0300

    Added a basic UI.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,82 @@
-import Project from './newProject.js';
+import Project from './newProject';
+import './styles.css';
 // create the default project that will show when the app is launched for the first time.
 const main = (() => {
+  // an array holding all projects.
   const projectArray = [];
   const defaultProject = new Project('default');
-  return { projectArray, defaultProject };
+  // project creation.
+  const createProject = (projectName) => {
+    const newProject = new Project(projectName);
+    projectArray.push(newProject);
+    return newProject;
+  };
+
+  const deleteProject = (projectName) => {
+    const indexToDelete = projectArray.findIndex((task) => task === projectName);
+    if (indexToDelete === -1) {
+      return;
+    }
+    projectArray.splice(indexToDelete, 1);
+    console.log('project deleted!');
+  };
+
+  return {
+    projectArray, defaultProject, createProject, deleteProject,
+  };
 })();
 
-console.log(main.defaultProject);
+// displays the newly created projects.
+const displayProjects = (projectName) => {
+  const projectArea = document.querySelector('.projectArea');
+  const projectElement = document.createElement('div');
+  projectElement.textContent = projectName;
+  projectArea.appendChild(projectElement);
+};
+
+// creates a project via form.
+const projectCreationDOM = (() => {
+  const createProjectButt = document.getElementById('createProjectButt');
+  const projectForm = document.getElementById('createProjectForm');
+  const projectFormContainer = document.getElementById('projectFormContainer');
+
+  // the 'create project' pop-up.
+  createProjectButt.addEventListener('click', () => {
+    if (projectForm.style.display === 'none' && projectFormContainer.style.display === 'none') {
+      projectForm.style.display = 'flex';
+      projectFormContainer.style.display = 'flex';
+    } else {
+      projectForm.style.display = 'none';
+      projectFormContainer.style.display = 'none';
+    }
+  });
+
+  projectForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // get input
+    const formInput = document.getElementById('project_name');
+    if (formInput.value === '') {
+      alert('must not be empty!');
+    }
+    const newProject = main.createProject(formInput.value);
+    // close the pop-up
+    projectForm.style.display = 'none';
+    projectFormContainer.style.display = 'none';
+    displayProjects(formInput.value);
+  });
+})();
+
+// const newP = main.createProject('helloWorld');
+// newP.createTodo('kaki', 'pipi', 'tusik', 'ichs');
+
+// console.log(newP);
+// console.log(main.projectArray);
+
+// // main.deleteProject(newP);
+
+// const test = main.defaultProject.createTodo('kaki', 'kaki1', 'kaki2', 'kaki3');
+
+// main.defaultProject.deleteTodo(test);
+
+// console.log(main.defaultProject);
